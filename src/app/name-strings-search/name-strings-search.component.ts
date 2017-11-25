3f6f9031a1d1b2205c38ae46d736122cb54947e6
Alexander Myltsev <alexander@myltsev.com>
2017-11-25 10:12:00 +0300

    Add `search` query param

--- src/app/name-strings-search/name-strings-search.component.ts
+++ src/app/name-strings-search/name-strings-search.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ApiClientService} from '../api-client/api-client.service';
 import {NameStringEntry} from '../api-client/name-string-entry';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-name-strings-search',
@@ -9,19 +10,35 @@ import {NameStringEntry} from '../api-client/name-string-entry';
   providers: [ApiClientService]
 })
 export class NameStringsSearchComponent implements OnInit {
+  searchParamName = 'search';
   searchText = '';
   results: Array<NameStringEntry> = [];
 
-  constructor(private _apiClientService: ApiClientService) {
+  constructor(private _apiClientService: ApiClientService,
+              private _activatedRoute: ActivatedRoute,
+              private _router: Router) {
   }
 
   ngOnInit() {
+    this._activatedRoute.queryParams.subscribe((params: Params) => {
+      this.searchText = params[this.searchParamName];
+      if (this.searchText && this.searchText.length > 0) {
+        this.update();
+      }
+    });
   }
 
   search() {
+    const queryParams: Params = Object.assign({}, this._activatedRoute.snapshot.queryParams);
+    queryParams[this.searchParamName] = this.searchText;
+    this._router.navigate(['.'], {queryParams: queryParams});
+  }
+
+  update() {
     this._apiClientService.searchNameStrings(this.searchText)
       .subscribe((nses: Array<NameStringEntry>) => {
-        this.results = nses;
+        this.results = nses.slice(0, 30);
+        console.log('name-strings results:')
         console.log(this.results);
       });
   }
