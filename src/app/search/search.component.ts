017fcc38d3aa537d3ebe5040d78b91c46fadcb57
Alexander Myltsev <alexander@myltsev.com>
2018-02-04 14:28:00 +0300

    Add name-filter to search component

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {SearchStatusService} from '../search-box/search-box.service';
+import {SearchStatus, SearchStatusService} from '../search-box/search-box.service';
+import {ActivatedRoute, Params} from '@angular/router';
 
 export enum SearchState {
   Empty = 1,
@@ -17,17 +18,28 @@ export class SearchComponent implements OnInit {
 
   searchState: SearchState = SearchState.Empty;
 
-  constructor(private _searchStatusService: SearchStatusService) {
+  constructor(private _searchStatusService: SearchStatusService,
+              private _activatedRoute: ActivatedRoute) {
     this._searchStatusService.searchStatus$.subscribe(searchStatus => {
-      if (searchStatus.searchText.length === 0) {
-        this.searchState = SearchState.Empty;
-      } else if (searchStatus.isNameFilterRequest()) {
-        this.searchState = SearchState.NameFilter;
-      } else {
-        this.searchState = SearchState.NameResolver;
-      }
-      console.log(this.searchState);
+      this.updateSearchState(searchStatus);
     });
+
+    this._activatedRoute.queryParams.subscribe((params: Params) => {
+      const searchText = params['q'];
+      const ss = new SearchStatus(searchText);
+      this.updateSearchState(ss);
+    });
+  }
+
+  updateSearchState(searchStatus: SearchStatus) {
+    if (searchStatus.searchText.length === 0) {
+      this.searchState = SearchState.Empty;
+    } else if (searchStatus.isNameFilterRequest()) {
+      this.searchState = SearchState.NameFilter;
+    } else {
+      this.searchState = SearchState.NameResolver;
+    }
+    console.log('search state: ' + this.searchState);
   }
 
   ngOnInit() {
